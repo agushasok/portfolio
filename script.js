6777838505360413a56a3e8c4e584640120c004b
Марков Даниил <daniil.markov@simbirsoft.com>
2021-09-30 17:21:28 +0300

    init

---
+++ script.js
@@ -0,0 +1,20 @@
+document.querySelectorAll('.item').forEach(item => {
+    item.addEventListener('click', () => {
+        const activeEl = document.querySelector('.item.active');
+        if (activeEl === item) return;
+
+        setTimeout(() => {
+            activeEl.querySelector('.content').classList.remove('content-bordered');
+        }, 760)
+        activeEl.classList.remove('active');
+        item.querySelector('.content').classList.add('content-bordered');
+        item.classList.add('active');
+    })
+});
+
+const marqueeEl = document.querySelector('.marquee');
+const marqueeTextEl = marqueeEl.querySelector('h2');
+
+for (let i = 0; i <= Math.ceil(window.innerWidth / marqueeTextEl.clientWidth); i++) {
+    marqueeEl.append(marqueeTextEl.cloneNode(true));
+}
