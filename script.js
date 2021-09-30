13932dc71e748bf630a0e8461515bd8a2b2a8cac
Марков Даниил <daniil.markov@simbirsoft.com>
2021-09-30 21:52:02 +0300

    add some projects

--- script.js
+++ script.js
@@ -9,6 +9,18 @@ document.querySelectorAll('.item').forEach(item => {
         activeEl.classList.remove('active');
         item.querySelector('.content').classList.add('content-bordered');
         item.classList.add('active');
+
+        if (item.querySelector('p').innerText === 'Работы' ) {
+            item.querySelectorAll('.cover').forEach((elem, i) => setTimeout(() => {
+                elem.classList.add('cover-animation');
+            }, (i + 1) * 200));
+        }
+
+        if (activeEl.querySelector('p').innerText === 'Работы') {
+            activeEl.querySelectorAll('.cover').forEach((elem, i) => setTimeout(() => {
+                elem.classList.remove('cover-animation');
+            }, (i + 1) * 200));
+        }
     })
 });
 
